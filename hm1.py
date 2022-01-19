fd79de217064dcf9d0d87bb570236fae2f1f609b
AlisaButska <buc.alisa@gmail.com>
2022-01-19 14:55:04 -0500

    Create hm1.py
    initial commit

---
+++ hm1.py
@@ -0,0 +1,33 @@
+# Создать две переменные first=10, second=30. Вывести на экран
+# построчно результат математического взаимодействия (+, -, *,
+# /, **) для этих чисел.
+
+first = 10
+second = 30
+
+sum_of_two = first + second
+sub_of_two_v1 = first - second
+sub_of_two_v2 = second - first
+mult_of_two = first * second
+div_of_two_v1 = first / second
+div_of_two_v2 = second / first
+exp_v1 = first ** second
+exp_v2 = second ** first
+
+int_div_of_two_v1 = first // second
+int_div_of_two_v2 = second // first
+modulo_v1 = first % second
+modulo_v2 = second % first
+
+print(sum_of_two)
+print(sub_of_two_v1)
+print(sub_of_two_v2)
+print(mult_of_two)
+print(div_of_two_v1)
+print(div_of_two_v2)
+print(exp_v1)
+print(exp_v2)
+print(int_div_of_two_v1)
+print(int_div_of_two_v2)
+print(modulo_v1)
+print(modulo_v2)
