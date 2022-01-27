c7352598b4c2c0855b4d87ac66dae11b33763c7b
AlisaButska <buc.alisa@gmail.com>
2022-01-26 21:23:02 -0500

    Create hm2.py
    homework #2

---
+++ hm2.py
@@ -0,0 +1,17 @@
+# 1
+input_value = input()
+print('You entered: ', input_value, 'Your result is: ', int(input_value) * 2)
+
+##############################################################
+
+# 2
+# input_value = input()
+# calculated_value = float(input_value) * 2
+# print(f'You entered: {input_value}; Your result is: {calculated_value};')
+
+##############################################################
+
+# 3
+# input_value = input()
+# calculated_value = str(input_value) * 2
+# print('You entered: %s; Your result is: %s;' % (input_value, calculated_value))
